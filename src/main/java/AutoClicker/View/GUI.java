9ecd4d83ed3b202f9d0ec800bddd5f9d5759f013
Rehaan99 <rehaanmp@hotmail.co.uk>
2022-12-22 06:22:10 +0000

    Add initial settings panel options and display settings button better

--- src/main/java/AutoClicker/View/GUI.java
+++ src/main/java/AutoClicker/View/GUI.java
@@ -12,29 +12,51 @@ public class GUI {
     private final JButton KPButton = new JButton("Start Auto Button");
     private final JButton settingsButton = new JButton();
     private final JPanel functionsPanel = new JPanel();
+    private final JPanel settingsPanel = new JPanel();
+    private final JTextField autoClickHotKeyStart = new JTextField();
+    private final JFrame window = new JFrame();
+
 
     public GUI() {
         ImageIcon settingsIcon = new ImageIcon("resources/settingsImage.png");
         Image image = settingsIcon.getImage();
-        image = image.getScaledInstance(10, 10, Image.SCALE_SMOOTH);
+        image = image.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
         settingsIcon = new ImageIcon(image);
         intervalACTextField.setText("1000");
         intervalKPTextField.setText("1000");
         JLabel intervalLabel = new JLabel("Auto Clicker:");
         JLabel keyPressLabel = new JLabel("Auto Button:");
         KPButton.setEnabled(false);
+
+        autoClickHotKeyStart.setOpaque(false);
+        autoClickHotKeyStart.setPreferredSize(new Dimension(25,25));
+
         settingsButton.setIcon(settingsIcon);
-        settingsButton.setPreferredSize(new Dimension(10, 10));
+        settingsButton.setPreferredSize(new Dimension(25, 25));
         settingsButton.setBorderPainted(false);
         settingsButton.setContentAreaFilled(false);
         settingsButton.setFocusPainted(false);
         settingsButton.setOpaque(false);
 
+        settingsPanel.setBorder(BorderFactory.createEmptyBorder(10, 50, 50, 50));
+        settingsPanel.setLayout(new GridLayout(0, 3));
+        JLabel function = new JLabel("Function");
+        function.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel hotKey = new JLabel("Hot Key");
+        hotKey.setHorizontalAlignment(SwingConstants.LEFT);
+        settingsPanel.add(function);
+        settingsPanel.add(new JLabel(""));
+        settingsPanel.add(hotKey);
+        settingsPanel.add(new JLabel(""));
+        settingsPanel.add(new JLabel(""));
+        settingsPanel.add(new JLabel(""));
+        settingsPanel.add(new JLabel("AC Start"));
+        settingsPanel.add(new JLabel(""));
+        settingsPanel.add(autoClickHotKeyStart);
 
 
         functionsPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
         functionsPanel.setLayout(new GridLayout(0, 4));
-
         functionsPanel.add(new JLabel(""));
         functionsPanel.add(new JLabel("Interval (ms) 1000ms = 1s"));
         functionsPanel.add(new JLabel("Key Press"));
@@ -49,15 +71,18 @@ public class GUI {
         functionsPanel.add(keyPressTextField);
         functionsPanel.add(KPButton);
 
-        JFrame GUIPanel = new JFrame();
-        GUIPanel.setResizable(false);
-        GUIPanel.add(settingsButton,BorderLayout.LINE_END);
-        GUIPanel.add(functionsPanel, BorderLayout.CENTER);
-        GUIPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GUIPanel.setTitle("Auto Clicker");
-        GUIPanel.pack();
-        GUIPanel.setVisible(true);
-        GUIPanel.setAlwaysOnTop(true);
+        JPanel hotBar = new JPanel();
+        hotBar.setLayout(new GridBagLayout());
+        hotBar.add(settingsButton);
+
+        window.setResizable(false);
+        window.add(hotBar, BorderLayout.EAST);
+        window.add(functionsPanel);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Auto Clicker");
+        window.pack();
+        window.setVisible(true);
+        window.setAlwaysOnTop(true);
     }
     public JTextField getKeyPressTextField(){
         return keyPressTextField;
@@ -78,7 +103,13 @@ public class GUI {
         return settingsButton;
     }
 
+    public JPanel getSettingsPanel() {
+        return settingsPanel;
+    }
     public JPanel getFunctionsPanel() {
         return functionsPanel;
+    }
+    public JFrame getWindow(){
+        return window;
     }
 }
