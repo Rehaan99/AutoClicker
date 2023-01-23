75176229429367ed2db09c70d528d1ff0c35d46a
Rehaan99 <rehaanmp@hotmail.co.uk>
2023-01-23 01:25:41 +0000

    Make use of JavaFX and FXML for GUI

--- src/main/java/AutoClicker/Model/KeyPress.java
+++ src/main/java/AutoClicker/Model/KeyPress.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class KeyPress implements AutoFunction {
-    private static ArrayList<SwingWorkerListener> listeners = new ArrayList<>();
+    private static final ArrayList<SwingWorkerListener> listeners = new ArrayList<>();
     private SwingWorker<Void, Void> worker;
     private int interval;
     private int keyCode;
