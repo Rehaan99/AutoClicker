141d37457bac5ecfcf43c176a9eccc4e3cc5610b
Rehaan99 <rehaanmp@hotmail.co.uk>
2023-01-29 02:06:16 +0000

    Add second controller

---
+++ src/main/java/AutoClicker/Controller/overlayController.java
@@ -0,0 +1,21 @@
+package AutoClicker.Controller;
+
+import AutoClicker.Model.Observer;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class overlayController  implements Observer, Initializable {
+
+
+    @Override
+    public void update(boolean event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
